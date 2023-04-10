911e358b37dd4da3a2a4ffd69f5e3ee82b54bb8e
Akinbande Gbenga <olugbengalium@gmail.com>
2023-04-10 21:32:51 +0100

    Initial Commit

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by Gbenga Abayomi on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
